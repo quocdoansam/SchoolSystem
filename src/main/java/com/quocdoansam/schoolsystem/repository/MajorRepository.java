da105fdf9f2130d8f45125cd44f041405998175f
quocdoansam <quocdoansam.me@gmail.com>
2025-04-20 23:09:17 +0700

    fix: Change ID type from Long to String in MajorRepository

--- src/main/java/com/quocdoansam/schoolsystem/repository/MajorRepository.java
+++ src/main/java/com/quocdoansam/schoolsystem/repository/MajorRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.quocdoansam.schoolsystem.entity.Major;
 
-public interface MajorRepository extends JpaRepository<Major, Long> {
+public interface MajorRepository extends JpaRepository<Major, String> {
 
 }
