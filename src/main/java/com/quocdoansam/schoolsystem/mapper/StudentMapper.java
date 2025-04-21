c701574da303eb0d101920070cb7e43eb20e6ed8
quocdoansam <quocdoansam.me@gmail.com>
2025-04-21 23:39:26 +0700

    add method toStudentUpdateRequest and ignore some fields.

--- src/main/java/com/quocdoansam/schoolsystem/mapper/StudentMapper.java
+++ src/main/java/com/quocdoansam/schoolsystem/mapper/StudentMapper.java
@@ -2,9 +2,11 @@ package com.quocdoansam.schoolsystem.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import com.quocdoansam.schoolsystem.dto.request.StudentCreationRequest;
+import com.quocdoansam.schoolsystem.dto.request.StudentUpdateRequest;
 import com.quocdoansam.schoolsystem.dto.response.StudentResponse;
 import com.quocdoansam.schoolsystem.entity.Student;
 
@@ -20,7 +22,16 @@ public interface StudentMapper {
 	@Mapping(target = "hometown", ignore = true)
 	@Mapping(target = "id", ignore = true)
 	@Mapping(target = "roles", ignore = true)
+	@Mapping(target = "tuitionFees", ignore = true)
 	Student toStudentCreationRequest(StudentCreationRequest request);
+
+	@Mapping(target = "id", ignore = true)
+	@Mapping(target = "major", ignore = true)
+	@Mapping(target = "tuitionFees", ignore = true)
+	@Mapping(target = "createdAt", ignore = true)
+	@Mapping(target = "updatedAt", ignore = true)
+	@Mapping(target = "roles", ignore = true)
+	Student toStudentUpdateRequest(StudentUpdateRequest request, @MappingTarget Student student);
 
 	@Mapping(target = "majorName", source = "major.name")
 	StudentResponse toStudentResponse(Student student);
