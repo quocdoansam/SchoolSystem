a4662f4faf845eb5516c2c3ffcc12daaeb162958
quocdoansam <quocdoansam.me@gmail.com>
2025-04-23 10:47:05 +0700

    feat: Add methods to find salaries by teacher and month in SalaryRepository

--- src/main/java/com/quocdoansam/schoolsystem/repository/SalaryRepository.java
+++ src/main/java/com/quocdoansam/schoolsystem/repository/SalaryRepository.java
@@ -7,8 +7,13 @@ import org.springframework.stereotype.Repository;
 
 import com.quocdoansam.schoolsystem.entity.Salary;
 import com.quocdoansam.schoolsystem.entity.Teacher;
+import java.util.List;
 
 @Repository
 public interface SalaryRepository extends JpaRepository<Salary, String> {
     public boolean existsByTeacherAndMonthSalary(Teacher teacher, YearMonth yearMonth);
+
+    public List<Salary> findByTeacher(Teacher teacher);
+
+    public List<Salary> findByMonthSalary(YearMonth monthSalary);
 }
